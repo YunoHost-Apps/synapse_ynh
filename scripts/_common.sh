63e05d642565e6e9f9e5faae3addf3808b77ac49
Josué Tille <josue@tille.ch>
2024-08-22 19:56:50 +0200

    Fix install on bookworn

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,4 +1,4 @@
-dependances="coturn build-essential python3-dev libffi-dev python3-pip python3-setuptools sqlite3 libssl-dev python3-venv libxml2-dev libxslt1-dev python3-lxml zlib1g-dev libjpeg-dev libpq-dev postgresql acl"
+dependances="coturn build-essential python3-dev libffi-dev python3-pip python3-setuptools sqlite3 libssl-dev python3-venv libxml2-dev libxslt1-dev python3-lxml zlib1g-dev libjpeg-dev libpq-dev postgresql acl php8.3-fpm"
 python_version="$(python3 -V | cut -d' ' -f2 | cut -d. -f1-2)"
 app=$YNH_APP_INSTANCE_NAME
 
@@ -51,7 +51,7 @@ install_sources() {
         else
             pip3 install --upgrade setuptools wheel pip
         fi
-        
+
         chown $synapse_user:root -R $final_path
         sudo -u $synapse_user env PATH=$PATH pip3 install --upgrade 'cryptography>=3.4.7'
         pip3 install --upgrade cffi ndg-httpsclient psycopg2 lxml jinja2
